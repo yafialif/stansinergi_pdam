022a7d2febdcf39621c90f309846368adf66cd8f
Yafi Alif <yafialif01@gmail.com>
2023-05-27 17:01:41 +0700

    update 1

--- resources/views/admin/tagihanblanan/create.blade.php
+++ resources/views/admin/tagihanblanan/create.blade.php
@@ -46,7 +46,7 @@
 </div><div class="form-group">
     {!! Form::label('total_pemakaian', 'Total Pemakaian /M3*', array('class'=>'col-sm-2 control-label')) !!}
     <div class="col-sm-10">
-        {!! Form::text('total_pemakaian', old('total_pemakaian'), array('class'=>'form-control')) !!}
+        {!! Form::text('total_pemakaian', old('total_pemakaian'), array('class'=>'form-control','readonly'=>'true')) !!}
         
     </div>
 </div><div class="form-group">
